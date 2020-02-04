3905f5a544d23c412f6e7bc00de53ea0ef6cbd35
ETHAN LEO MING QUAN TP <1800249C@student.tp.edu.sg>
2020-02-04 22:10:06 +0800

    Add project files.

---
+++ SortingApplication/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SortingApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.lbSortedList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(46, 69);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(201, 22);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Add a number";
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.ItemHeight = 16;
+            this.lbNumbers.Location = new System.Drawing.Point(435, 82);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(201, 228);
+            this.lbNumbers.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(432, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number List";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sorted List";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(46, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Add Number";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(46, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSort.Location = new System.Drawing.Point(46, 215);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(201, 23);
+            this.btnSort.TabIndex = 8;
+            this.btnSort.Text = "Sort List";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // lbSortedList
+            // 
+            this.lbSortedList.FormattingEnabled = true;
+            this.lbSortedList.ItemHeight = 16;
+            this.lbSortedList.Location = new System.Drawing.Point(12, 353);
+            this.lbSortedList.Name = "lbSortedList";
+            this.lbSortedList.Size = new System.Drawing.Size(624, 212);
+            this.lbSortedList.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 570);
+            this.Controls.Add(this.lbSortedList);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbNumbers);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.ListBox lbSortedList;
+    }
+}
+
---
+++ SortingApplication/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace SortingApplication
+{
+    public partial class Form1 : Form
+    {
+        List<int> numberList = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var addNumber =Convert.ToInt32(numericUpDown1.Value);
+            numberList.Add(addNumber);
+            loadNumberlist();
+        }
+
+        private void loadNumberlist()
+        {
+            lbNumbers.DataSource = new BindingSource(numberList, null);
+        }
+
+        //clear sorted list and numbers to sort
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numberList.Clear();
+            lbSortedList.Items.Clear();
+            loadNumberlist();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+
+            if(numberList.Count() != 0)
+            {
+                var numArr = numberList.ToArray();
+
+               
+                var sortedList = "";
+                foreach (var i in numArr)
+                {
+                    if (sortedList == "")
+                    {
+                        sortedList += i.ToString();
+                    }
+                    else
+                    {
+                        sortedList += " ," + i.ToString();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please provide a list of numbers");
+            }
+
+        }
+        
+
+        class QSObj
+        {
+            public int NumberProp { get; set; }
+            public string StringProp { get; set; }
+            public double DoubleProp { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (StreamReader file = File.OpenText(@"D:\WORLD SKILLS\Notes,References & Resources\QuickSort\QuickSort.json"))
+            {
+                var json = file.ReadToEnd();
+
+                var qsobjList = JsonConvert.DeserializeObject<List<QSObj>>(json).ToArray();
+
+                var qsobjListOriginal = JsonConvert.DeserializeObject<List<QSObj>>(json);
+
+                //qsobjListOriginal.Sort();
+
+
+                var x = new List<QSObj>();
+                foreach (var i in qsobjListOriginal)
+                {
+                    //var qsObj = $@"{i.NumberProp}, {i.StringProp}, {i.NumberProp}";
+                    //Console.WriteLine(qsObj);
+                    x.Add(i);
+                }
+                Console.WriteLine(x.OrderByDescending(a => a.DoubleProp).FirstOrDefault().NumberProp);
+
+                //So logically, there are 3 properties that we must sort by
+                var properties = new List<string>() { "NumberProp", "StringProp", "DoubleProp" };
+
+                foreach(var prop in properties)
+                {
+                    quick_sort(qsobjList, 0, qsobjList.Length - 1, prop);
+                }
+
+                foreach(var item in qsobjList)
+                {
+                    var qsObj = $@"{item.NumberProp}, {item.StringProp}, {item.NumberProp}";
+
+                    lbSortedList.Items.Add(qsObj);
+                }
+            }
+
+        }
+
+        private static void quick_sort(QSObj[] numArr, int low, int high, string property)
+        {
+            if (property == "NumberProp")
+            {
+                if (low < high)
+                {
+                    var partIdx = partition(numArr, low, high);
+
+                    quick_sort(numArr, low, partIdx - 1 , property);
+                    quick_sort(numArr, partIdx + 1, high, property);
+                }
+            }
+            else if (property == "StringProp")
+            {
+                if (low < high)
+                {
+                    var partIdx = Stringpartition(numArr, low, high);
+
+                    quick_sort(numArr, low, partIdx - 1, property);
+                    quick_sort(numArr, partIdx + 1, high, property);
+                }
+            }
+            else
+            {
+                if (low < high)
+                {
+                    var partIdx = Doublepartition(numArr, low, high);
+
+                    quick_sort(numArr, low, partIdx - 1, property);
+                    quick_sort(numArr, partIdx + 1, high, property);
+                }
+            }
+
+            
+        }
+
+        private static int partition(QSObj[] numArr, int low, int high)
+        {
+            int pivot = numArr[high].NumberProp;
+
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                if (numArr[j].NumberProp < pivot)
+                {
+                    i++;
+
+                    QSObj tempNum = numArr[i];
+                    numArr[i] = numArr[j];
+                    numArr[j] = tempNum;
+                }
+            }
+
+            QSObj tempNum1 = numArr[i + 1];
+            numArr[i + 1] = numArr[high];
+            numArr[high] = tempNum1;
+
+            return i + 1;
+        }
+
+        private static int Stringpartition(QSObj[] numArr, int low, int high)
+        {
+            string pivot = numArr[high].StringProp;
+
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                var comparison = string.Compare(numArr[j].StringProp, pivot);
+
+                if (comparison == -1)
+                {
+                    i++;
+
+                    QSObj tempNum = numArr[i];
+                    numArr[i] = numArr[j];
+                    numArr[j] = tempNum;
+                }
+            }
+
+            QSObj tempNum1 = numArr[i + 1];
+            numArr[i + 1] = numArr[high];
+            numArr[high] = tempNum1;
+
+            return i + 1;
+        }
+
+        private static int Doublepartition(QSObj[] numArr, int low, int high)
+        {
+            double pivot = numArr[high].DoubleProp;
+
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+
+                if (numArr[j].DoubleProp < pivot)
+                {
+                    i++;
+
+                    QSObj tempNum = numArr[i];
+                    numArr[i] = numArr[j];
+                    numArr[j] = tempNum;
+                }
+            }
+
+            QSObj tempNum1 = numArr[i + 1];
+            numArr[i + 1] = numArr[high];
+            numArr[high] = tempNum1;
+
+            return i + 1;
+        }
+
+
+
+
+    }
+}
